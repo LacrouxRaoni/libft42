2fdae294edbbd332964bb994683354ba8c66c58a
LacrouxRaoni <lacroux.raoni@yahoo.com.br>
2021-08-16 23:55:03 -0300

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rruiz-la <rruiz-la@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/11 22:24:21 by rruiz-la          #+#    #+#             */
-/*   Updated: 2021/08/16 23:43:06 by rruiz-la         ###   ########.fr       */
+/*   Updated: 2021/08/16 23:53:09 by rruiz-la         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
-	size_t	i;
+	unsigned char	*s1;
+	unsigned char	*s2;
+	size_t			i;
 
+	s1 = (unsigned char *)str1;
+	s2 = (unsigned char *)str2;
 	i = 0;
 	if (n == 0)
 	{
@@ -23,13 +27,13 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	}
 	while (i < n)
 	{
-		if (str1[i] == '\0' && str2[i] == '\0')
+		if (s1[i] == '\0' && s2[i] == '\0')
 		{
 			return (0);
 		}
-		if (str1[i] != str2[i])
+		if (s1[i] != s2[i])
 		{
-			return ((int)(str1[i] - str2[i]));
+			return ((int)(s1[i] - s2[i]));
 			i++;
 		}
 	}
