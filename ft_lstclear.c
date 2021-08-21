d50637adfc5dea506c694dcc5e5ee3f1e19c5e4b
LacrouxRaoni <lacroux.raoni@yahoo.com.br>
2021-08-20 22:01:38 -0300

    clear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: rruiz-la <rruiz-la@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 18:52:16 by rruiz-la          #+#    #+#             */
-/*   Updated: 2021/08/20 21:32:06 by rruiz-la         ###   ########.fr       */
+/*   Updated: 2021/08/20 21:56:51 by rruiz-la         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,18 @@
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*curr;
-	t_list	*temp;
+	t_list	*prev;
 
 	if (!lst || !del)
-	{
 		return ;
-	}
 	curr = *lst;
 	while (curr)
 	{
-		temp = curr;
+		prev = curr;
 		curr = curr->next;
-		if (temp->content)
-		{
-			del(temp);
-		}
-		free(temp);
+		if (prev->content)
+			del(prev->content);
+		free(prev);
 	}
 	*lst = NULL;
 }
